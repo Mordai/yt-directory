93ca3f6e212521a77bed0db02bbde22fab0fbf85
Lengyel Balázs <lengyel.balazs12@gmail.com>
2025-01-17 17:41:26 +0100

    init

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,52 @@
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default async function Navbar() {
+  const session = await auth();
+
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+
+        <div className="flex item-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href={"/startup/create"}>
+                <span>Create</span>
+              </Link>
+
+              <form
+                onClick={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">Log out</button>
+              </form>
+
+              <Link href={`/user/${session?.user}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <>
+              <form
+                action={async () => {
+                  "use server";
+                  await signIn("github");
+                }}
+              >
+                <button type="submit">Sign in</button>
+              </form>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+}
