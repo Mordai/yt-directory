ffb07c1f68b30c8eef3e6ffbbb29262da5e34de1
Lengyel Balázs <lengyel.balazs12@gmail.com>
2025-01-18 20:55:41 +0100

    added SearchForm and Sanity

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,11 @@
-import Navbar from "@/app/components/Navbar";
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <main className="font-work-sans">
+    <main>
       <Navbar />
       {children}
     </main>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,26 @@
-import SearchForm from "@/app/components/SearchForm";
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const posts = [
+    {
+      createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Adrian" },
+      _id: 1,
+      descriptions: "Test description",
+      image:
+        "https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "We Robot",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -14,7 +34,21 @@ export default function Home() {
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
           Competitions.
         </p>
-        <SearchForm />
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?.id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No tartups Found</p>
+          )}
+        </ul>
       </section>
     </>
   );
